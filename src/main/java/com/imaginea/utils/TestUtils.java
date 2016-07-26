e574691d6c5b5a05634d86577e422b2f43d9e9a3
Nellore <krishnakumar.nellore@imaginea.com>
2016-07-26 16:22:39 +0530

    Add Page Object for Database section and updated Loginpage flow

--- src/main/java/com/imaginea/utils/TestUtils.java
+++ src/main/java/com/imaginea/utils/TestUtils.java
@@ -1,36 +1,38 @@
 package com.imaginea.utils;
 
 import java.io.File;
-import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.lang3.SystemUtils;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.WebElement;
 
 public class TestUtils {
 	private final Logger log = LogManager.getLogger(TestUtils.class.getName());
-	
+
 	public static String getDriverPath(String browser) {
 		String osName = SystemUtils.OS_NAME.toLowerCase();
 		String driverName = getDriverName(browser);
 
 		String driverPath = "drivers" + File.separator;
 		if (osName.indexOf("win") >= 0) {
-		//	driverPath +=  File.separator + driverName + ".exe";
-			driverPath= System.getProperty("user.dir") + File.separator
-					+ "Drivers" + File.separator+ driverName + ".exe";
+			// driverPath += File.separator + driverName + ".exe";
+			driverPath = System.getProperty("user.dir") + File.separator
+					+ "Drivers" + File.separator + driverName + ".exe";
 			System.out.println(driverPath);
 		} else if (osName.indexOf("mac") >= 0) {
 			driverPath += File.separator + driverName;
 		} else {
-			driverPath +=  File.separator + driverName;
+			driverPath += File.separator + driverName;
 		}
 
 		URL u1 = TestUtils.class.getClassLoader().getResource(driverPath);
-		System.out.println("URL: {}"  +u1);
-		
-		System.out.println("driverPath.."+driverPath);
+		System.out.println("URL: {}" + u1);
+
+		System.out.println("driverPath.." + driverPath);
 		return driverPath;
 	}
 
@@ -44,5 +46,18 @@ public class TestUtils {
 		}
 	}
 
-
+	/**
+	 * Get List of webelement in String
+	 * 
+	 * @param element
+	 * @return
+	 */
+	public static List<String> convertWebElementListToString(
+			List<WebElement> element) {
+		List<String> list = new ArrayList<>();
+		for (WebElement e : element) {
+			list.add(e.getText());
+		}
+		return list;
+	}
 }
--- src/test/java/com/imaginea/tests/SampleTest.java
+++ src/test/java/com/imaginea/tests/SampleTest.java
@@ -1,12 +1,13 @@
 package com.imaginea.tests;
 
-
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.testng.log4testng.Logger;
 
 import com.imaginea.base.BaseTest;
+import com.imaginea.pageObjects.DatabaseTab;
 import com.imaginea.pageObjects.LoginPage;
 
 public class SampleTest extends BaseTest {
@@ -14,16 +15,23 @@ public class SampleTest extends BaseTest {
 	WebDriver driver;
 	Logger log =Logger.getLogger(SampleTest.class); 
 	LoginPage loginPage;
+	DatabaseTab dbTab;
 	@BeforeClass
 	public void beforeClass(){
 		driver=getDriver();
 	}
+	
 	@Test
 	public void launch(){
-		log.debug("Running test case ....");
-		/*driver.get("http://mvnrepository.com/artifact/org.testng/testng/6.9.10");*/
-		driver.get("http://172.16.55.67:8081/index.html");
-		loginPage= new LoginPage(driver);
-		loginPage.clickGoButton();
+	    loginPage= PageFactory.initElements(driver, LoginPage.class);		 
+	    loginPage.loginmViewer();
+	    dbTab =  PageFactory.initElements(driver, DatabaseTab.class);
+	    String testDB=null;
+	    for (int i = 0; i < 10; i++) {
+	        testDB = "tet"+i;
+	        dbTab.createDb(testDB);
+	        dbTab.getDBList(driver).contains(testDB);
+            }
+	    
 	}
 }
